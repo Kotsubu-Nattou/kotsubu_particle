0cfbe92997ca4ee2ac0c13d9d7c5f59c637854a4
kotsubu <@desktop>
2019-04-27 13:57:37 +0900

    修正及び追加

--- particle/Main.cpp
+++ particle/Main.cpp
@@ -4,16 +4,16 @@
 
 void Main()
 {
-    Particle::CircleShadow particle;
-
+    Particle::Smoke particle;
+    
     while (System::Update()) {
         if (MouseL.pressed()) {
             int x = Cursor::Pos().x;
             int y = Cursor::Pos().y;
 
-            particle.pos(Vec2(x, y)).size(7).speed(5);
+            particle.pos(Vec2(x, y)).size(20).speed(3);
             particle.angle(0).angleRange(360);
-            particle.color(Color(255, 220, 100, 150)).create(10);
+            particle.color(ColorF(1.0, 0.85, 0.4, 0.1)).create(30);
         }
         
         particle.update();
--- particle/Particle.h
+++ particle/Particle.h
@@ -16,17 +16,19 @@ namespace
 
 namespace Particle
 {
-    class Circle
+    // 【基底クラス】ふつうのパーティクル
+    // すべてのパーティクルの基礎となるクラス。他のパーティクルはこれを拡張（継承）したもの
+    class Plain
     {
     protected:
         // クラス内部で使用する構造体
-        struct Obj
+        struct Element
         {
-            Obj() :
+            Element() :
                 pos(Vec2(0, 0)), size(1.0), radian(0.0), speed(5.0),
                 color(ColorF(1.0, 0.9, 0.6, 0.8)), gravity(0.0), enable(true)
             {}
-            Obj(Vec2 _pos, double _size, double _radian, double _speed, ColorF _color) :
+            Element(Vec2 _pos, double _size, double _radian, double _speed, ColorF _color) :
                 pos(_pos), size(_size), radian(_radian), speed(_speed), color(_color),
                 gravity(0.0), enable(true)
             {}
@@ -40,9 +42,9 @@ namespace Particle
         };
 
 
-        struct Status : public Obj
+        struct ElementProperty : public Element
         {
-            Status() :
+            ElementProperty() :
                 randPow(5.0), accelSpeed(-0.1), accelSize(-0.1),
                 accelColor(0.0, -0.02, -0.03, -0.005),
                 gravityPow(0.2), gravityRad(Pi / 2.0),
@@ -59,26 +61,26 @@ namespace Particle
         };
 
         // 【フィールド】
-        Status status;
-        std::vector<Obj> obj;
+        ElementProperty property;
+        std::vector<Element> elements;
     
 
 
     public:
         // 【コンストラクタ】
-        Circle(size_t reserve = 3000)
+        Plain(size_t reserve = 3000)
         {
-            obj.reserve(reserve);
+            elements.reserve(reserve);
         }
     
 
         // 【セッタ】各初期パラメータ。メソッドチェーン方式
-        Circle& pos(  Vec2   pos)   { status.pos   = pos;   return *this; }
-        Circle& size( double size)  { status.size  = size;  return *this; }
-        Circle& speed(double speed) { status.speed = speed; return *this; }
-        Circle& color(Color  color) { status.color = color; return *this; }
+        Plain& pos(  Vec2   pos)   { property.pos   = pos;   return *this; }
+        Plain& size( double size)  { property.size  = size;  return *this; }
+        Plain& speed(double speed) { property.speed = speed; return *this; }
+        Plain& color(Color  color) { property.color = color; return *this; }
 
-        Circle& angle(double angle)
+        Plain& angle(double angle)
         {
             if (angle < 0.0) {
                 angle = fmod(angle, 360.0) + 360.0;
@@ -87,16 +89,16 @@ namespace Particle
             else if (angle >= 360.0)
                 angle = fmod(angle, 360.0);
         
-            status.radian = angle * PiDivStraight;
+            property.radian = angle * PiDivStraight;
             return *this;
         }
 
-        Circle& angleRange(double angleRange)
+        Plain& angleRange(double angleRange)
         {
             if (angleRange < 0.0) angleRange = 0.0;
             if (angleRange > 360.0) angleRange = 360.0;
 
-            status.radianRange = angleRange * PiDivStraight;
+            property.radianRange = angleRange * PiDivStraight;
             return *this;
         }
 
@@ -108,19 +110,19 @@ namespace Particle
 
             for (int i = 0; i < quantity; ++i) {
                 // サイズ
-                fix  = status.size * status.randPow / 30.0;
-                size = status.size + Random(-fix, fix);
+                fix  = property.size * property.randPow / 30.0;
+                size = property.size + Random(-fix, fix);
                 if (size < 0.5) size = 0.5;
 
                 // 角度
-                fix = Random(status.radianRange) - status.radianRange / 2.0;
-                rad = fmod(status.radian + fix + TwoPi, TwoPi);
+                fix = Random(property.radianRange) - property.radianRange / 2.0;
+                rad = fmod(property.radian + fix + TwoPi, TwoPi);
 
                 // スピード
-                speed = status.speed + Random(-status.randPow, status.randPow);
+                speed = property.speed + Random(-property.randPow, property.randPow);
 
                 // 要素を追加
-                obj.emplace_back(Obj(status.pos, size, rad, speed, status.color));
+                elements.emplace_back(Element(property.pos, size, rad, speed, property.color));
             }
         }
 
@@ -131,20 +133,20 @@ namespace Particle
         {
             double windowWidth  = Window::Width();
             double windowHeight = Window::Height();
-            double gravitySin = sin(status.gravityRad);
-            double gravityCos = cos(status.gravityRad);
+            double gravitySin = sin(property.gravityRad);
+            double gravityCos = cos(property.gravityRad);
 
-            for (auto &r : obj) {
+            for (auto &r : elements) {
                 // 色の変化
-                r.color += status.accelColor;  // ColorFを「+=」した場合、対象はRGBのみ
-                r.color.a += status.accelColor.a;
-                if (r.color.a < 0.05) {
+                r.color += property.accelColor;  // ColorFを「+=」した場合、対象はRGBのみ
+                r.color.a += property.accelColor.a;
+                if (r.color.a < 0.01) {
                     r.enable = false;
                     continue;
                 }
 
                 // サイズの変化
-                r.size += status.accelSize;
+                r.size += property.accelSize;
                 if (r.size < 0.1) {
                     r.enable = false;
                     continue;
@@ -155,7 +157,7 @@ namespace Particle
                 r.pos.y += sin(r.radian) * r.speed;
 
                 // 引力
-                r.gravity += status.gravityPow;
+                r.gravity += property.gravityPow;
                 r.pos.x += gravityCos * r.gravity;
                 r.pos.y += gravitySin * r.gravity;
 
@@ -167,16 +169,16 @@ namespace Particle
                 }
 
                 // スピードの変化
-                r.speed += status.accelSpeed;
+                r.speed += property.accelSpeed;
                 if (r.speed < 0.0) r.speed = 0.0;
             }
 
             // 無効な粒子を削除
-            auto dustIt = std::remove_if(obj.begin(), obj.end(),
-                [](Obj &obj) { return !obj.enable; });
-            obj.erase(dustIt, obj.end());
+            auto dustIt = std::remove_if(elements.begin(), elements.end(),
+                [](Element &element) { return !element.enable; });
+            elements.erase(dustIt, elements.end());
 
-            Print << U"obj.size: " << obj.size();
+            Print << U"elements.size: " << elements.size();
         }
 
 
@@ -184,9 +186,9 @@ namespace Particle
         // 【メソッド】ドロー
         void draw()
         {
-            s3d::RenderStateBlock2D tmp(status.blendState);  // tmpが生きている間だけ設定が有効。破棄時に設定は元に戻る
+            s3d::RenderStateBlock2D tmp(property.blendState);  // tmpが生きている間だけ有効。破棄時に元に戻る
         
-            for (auto &r : obj)
+            for (auto &r : elements)
                 s3d::Circle(r.pos, r.size).draw(r.color);
         }
     };
@@ -195,19 +197,39 @@ namespace Particle
 
 
 
-    class CircleShadow : public Circle
+    // 【派生クラス】微光のパーティクル（なめらかだが重い）
+    class Light : public Plain
     {
     public:
         // 【メソッド】ドロー（オーバーライド）
         void draw()
         {
-            s3d::RenderStateBlock2D tmp(status.blendState);  // tmpが生きている間だけ設定が有効。破棄時に設定は元に戻る
+            s3d::RenderStateBlock2D tmp(property.blendState);
 
-            for (auto &r : obj)
+            for (auto &r : elements)
                 s3d::Circle(r.pos, r.size).drawShadow(Vec2(0, 0), 10.0, 2.0, r.color);
         }
     };
+
+
+
+
+
+    // 【派生クラス】煙のパーティクル
+    class Smoke : public Plain
+    {
+    public:
+        // 【メソッド】ドロー（オーバーライド）
+        void draw()
+        {
+            double fix;
+            s3d::RenderStateBlock2D tmp(property.blendState);
+
+            for (int i = 0; i < 5; ++i) {
+                fix = i * 0.2;
+                for (auto &r : elements)
+                    s3d::Circle(r.pos, r.size - r.size * fix).draw(r.color);
+            }
+        }
+    };
 }
-
-
-// enumを用意し、関数ポインタ配列にラムダ式を入れたほうがいいかも（クラス名もParticleにできる。コンストラクタでenumを定義させる）
